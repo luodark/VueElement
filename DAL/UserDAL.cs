632256131dc9656bb5ba0a04b4313f0c6d3e7866
luodark <312904636@qq.com>
2018-02-02 17:57:02 +0800

    1

---
+++ DAL/UserDAL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DAL
+{
+    /// <summary>
+    /// 用户数据访问类
+    /// </summary>
+    public class UserDal
+    {
+        /// <summary>
+        /// 获取所有用户信息
+        /// </summary>
+        /// <returns>所有用户列表</returns>
+        public static List<LoginUser> GetUserList()
+        {
+            var userList = new List<LoginUser>
+            {
+                new LoginUser
+                {
+                    UserName = "admin",
+                    Password = "123456",
+                    RealName = "管理员",
+                    Mobile = "18888888888"
+                }
+            };
+            for (var i = 0; i < 995; i++)
+            {
+                userList.Add(new LoginUser
+                {
+                    UserName = "UserName"+i,
+                    Password = "Password" + i,
+                    RealName = "RealName" + i,
+                    Mobile = "Mobile" + i
+                });
+            }
+            return userList;
+        }
+    }
+}
--- Model/LoginUser.cs
+++ Model/LoginUser.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Model
 {
+    /// <summary>
+    /// 用户登录实体
+    /// </summary>
     public class LoginUser
     {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 真实姓名
+        /// </summary>
+        public string RealName { get; set; }
+
+        /// <summary>
+        /// 联系电话
+        /// </summary>
+        public string Mobile { get; set; }
     }
 }
